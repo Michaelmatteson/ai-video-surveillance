0729ba8a80900033002464346af71b2feb93b597
Michaelmatteson <93003020+Michaelmatteson@users.noreply.github.com>
2022-09-12 09:37:44 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+objects = [];
+status = "";
 video = "";
 
 
@@ -14,6 +16,21 @@ function setup(){
 
 function draw(){
    image(video, 0, 0, 480, 380);
+       if(status != "")
+       {
+        objectDetector.detect(video, gotresult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML == "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML == "Number of objects detected are : "+ objects.length;
+
+            fill("f0f0f0");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("f0f0f0");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height,);
+        }
+       }
 }
 
 function start()
@@ -28,4 +45,12 @@ function modelLoaded() {
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
